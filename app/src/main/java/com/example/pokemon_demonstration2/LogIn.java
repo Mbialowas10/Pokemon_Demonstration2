3ed59703253b17e8c3d5399313901ff6f7316787
mbialowas10 <mbialowas@rrc.ca>
2023-02-10 16:42:40 -0600

    Added Login Form components

--- app/src/main/java/com/example/pokemon_demonstration2/LogIn.java
+++ app/src/main/java/com/example/pokemon_demonstration2/LogIn.java
@@ -3,12 +3,36 @@ package com.example.pokemon_demonstration2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LogIn extends AppCompatActivity {
+    EditText email;
+    EditText password;
+    Button btn_sign_in, btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+        btn_register = findViewById(R.id.btn_register);
+
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
