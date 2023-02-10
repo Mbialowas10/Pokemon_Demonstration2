a4c71cecbde65e1af2f1afafff68e789e8cf87d2
mbialowas10 <mbialowas@rrc.ca>
2023-02-10 17:30:52 -0600

    Register User on FB Integration

--- app/src/main/java/com/example/pokemon_demonstration2/LogIn.java
+++ app/src/main/java/com/example/pokemon_demonstration2/LogIn.java
@@ -1,5 +1,6 @@
 package com.example.pokemon_demonstration2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,11 +8,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogIn extends AppCompatActivity {
     EditText email;
     EditText password;
+    String email_str;
+    String pw_str;
     Button btn_sign_in, btn_register;
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,9 @@ public class LogIn extends AppCompatActivity {
         password = findViewById(R.id.password);
         btn_sign_in = findViewById(R.id.btn_sign_in);
         btn_register = findViewById(R.id.btn_register);
+        auth = FirebaseAuth.getInstance();
+
+
 
         btn_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,12 +45,45 @@ public class LogIn extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        // create a new user account in FB
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
+                email_str  = email.getText().toString();
+                pw_str = password.getText().toString();
+                createUserAccount(email_str, pw_str);
             }
         });
     }
+    public void createUserAccount(String email_str, String pw_str){
+        auth.createUserWithEmailAndPassword(email_str, pw_str)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(LogIn.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+    /*
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser != null) {
+            reload();
+        }
+    }
+    */
+
+
 }
